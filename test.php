6d7325510137f3ff75040e1bccdc636ae5bc3686
edoardo <collo>
2021-01-28 23:19:29 +0100

    test

--- test.php
+++ test.php
@@ -1,7 +1,9 @@
 <?php
 $nome = $_GET['nome'];
 $cognome = $_GET['cognome'];
-
+$data = date('y/m/d');
+$ora = date('H:i:s');
+$t = date('s');
 ?>
 
 
@@ -13,5 +15,60 @@ $cognome = $_GET['cognome'];
   </head>
   <body>
     <h2>ciao <?php echo $nome . ' ' . $cognome; ?></h2>
+    <h3><?php echo $data. ' '. $ora;?></h3>
+    <h1>
+      <?php
+      echo $t;
+      if ($t > 40){
+        echo 'boom';
+      } else if ($t > 20){
+        echo 'bim';
+      }else{
+        echo 'bam';
+      }
+      ?>
+    </h1>
+    <h2>
+      <?php
+        switch ($t){
+          case $t > 20 :
+            echo 'bim';
+            break;
+          case $t > 40 :
+            echo 'boom';
+            break;
+          default :
+            echo 'bam';
+
+        }
+       ?>
+    </h2>
+    <p>
+      <?php
+        $x = 0;
+        $y = 0;
+        while ($x<=5){
+          echo 'il numero dtampato e:'. $x . '<br>';
+          $x++;
+        };
+        do {
+          echo $y . '<br>';
+          $y++;
+        } while ($y <= 10);
+        for ($i=0; $i <= 10; $i++) {
+          echo $i * 10 . '<br>';
+        }
+       ?>
+    </p>
+
+    <ul>
+      <?php
+        $list = array('lorem','lorem1','lorem2','lorem3','lorem4','lorem5');
+        foreach ($list as $value) {
+          // echo '<li>' . $value . '</li>'
+          echo '<li>'.$value.'</li>';
+        };
+       ?>
+    </ul>
   </body>
 </html>
