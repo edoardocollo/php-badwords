b1c27fed23dbb8ae495ba7732383b35d2295b5c2
edoardo <collo>
2021-01-28 15:51:50 +0100

    php end

---
+++ index.php
@@ -0,0 +1,19 @@
+<?php
+$testo = 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.';
+$parola = $_GET['parola'];
+
+?>
+
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title></title>
+  </head>
+  <body>
+    <h2>
+      <?php echo str_replace($parola, '***', $testo);?>
+  </h2>
+  <h3>il paragrafo e' lungo <?php echo strlen($testo);?> caratteri</h3>
+  </body>
+</html>
